45df3aa43118af9870840789809ccfedddf2fb66
pteribas <pteribas@gmail.com>
2026-03-17 15:54:40 +0100

    actualitzat

---
+++ taller_3/comandes_utilitzades.bash
@@ -0,0 +1,13 @@
+mkdir Fotografies
+mkdir Fotografies/Dia_de_Platja
+mkdir Fotografies/Viatge
+mkdir UIB_Labo
+mkdir UIB_Labo/taller_1
+mkdir UIB_Labo/taller_2
+mkdir UIB_Labo/taller_3
+
+touch UIB_Labo/taller_1/document.tex
+touch UIB_Labo/taller_3/operacions_math.py
+
+mv Fotografies/Platja/Foto_grup.jpg Fotografies/Dia_de_Platja/
+mv Fotografies/Viatge/Primer_dia.jpg Fotografies/Viatge/
